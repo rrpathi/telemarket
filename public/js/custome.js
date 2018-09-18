e8a73302f486cd252d305383c66808737f9dd916
spmohansp <spmohansp@gmail.com>
2018-09-18 11:18:12 +0530

    error

---
+++ public/js/custome.js
@@ -0,0 +1,88 @@
+$(document).ready(function(){
+	// customer change admin (get last data and remaining count)
+	$('body').on('change', '#exportCustomer', function() {
+		var exportCustomer = $("#exportCustomer option:selected").val();
+		$.ajax({
+			type : "get",
+			url:'/admin/customer_export_count',
+			data:{customer_id:exportCustomer},
+			success: function(data) {
+				// console.log(data);
+				if(data ==''){
+					$("#showTableData").html('');
+					$('#CustomerCount').val('');
+					$('#CustomerCount').attr('readonly', false);
+				}else{
+					if(data['count']!=''){
+						$("#showTableData").html(data.table);
+						$('#CustomerCount').val(data.count);
+						$('#CustomerCount').attr('readonly', true);
+					}else{
+						$("#showTableData").html('');
+						$('#CustomerCount').val('');
+						$('#CustomerCount').attr('readonly', false);
+					}
+				}
+			}
+		});
+	});
+
+
+
+	$(".category_change").change(function(){
+		var location = $("#location").val();
+		var category_value = $("#database_type").val();
+		$.ajax({
+			type : "get",
+			url:'/admin/catagory-change',
+			data:{location:location,category_value:category_value},
+			success: function(data) {
+				$('#optionData').html(data);
+				$('.export_change').trigger('change');
+			}
+		});
+	});
+
+
+
+
+	$('body').on('change', '.export_change', function() {
+		var vendor_code = $("#vendor_code").val();
+		var location = $("#location").val();
+		var database_type = $("#database_type").val();
+		var category_value = $("#category").val();
+		// console.log(category_value);
+		if (category_value!='') {
+			$('#vendor_code').show();
+		}
+		$.ajax({
+			type : "get",
+			url:'/admin/location-count',
+			data:{location:location,vendor_code:vendor_code,database_type:database_type,category_value:category_value},
+			success: function(data) {
+				$("#location_count").val(data);
+				$('#from_count').attr({"max":data});
+				$('#to_count').attr({"max":data});
+			}
+		});
+	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
